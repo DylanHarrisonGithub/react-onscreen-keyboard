cd6665b73e73a9f5503660933c43817d5f67f84f
Dylan Harrison <dylanharrison@Dylans-Mac-mini.attlocal.net>
2022-09-03 15:23:31 -0500

    react setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import OnscreenKeyboardComponent from './components/onscreen-keyboard/onscreen-keyboard-component';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div>
+      App Component
+      <OnscreenKeyboardComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/onscreen-keyboard/onscreen-keyboard-component.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const OnscreenKeyboardComponent: React.FC<any> = (props) => {
+  return (
+    <div>onscreen-keyboard-component</div>
+  )
+}
+export default OnscreenKeyboardComponent;
