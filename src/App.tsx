f50f8b110b5a8c91ac1c5ed39920de684c375234
Dylan Harrison <dylanharrison@Dylans-Mac-mini.attlocal.net>
2022-09-12 21:22:23 -0500

    demo gif

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,10 @@
 import React from 'react';
-import OnscreenKeyboardComponent from './components/onscreen-keyboard/onscreen-keyboard-component';
-import logo from './logo.svg';
-
+import { InputOnscreenComponent } from './components/input-onscreen/input-onscreen';
 function App() {
-
-  const [presses, setPresses] = React.useState<{ code: string, key: string }[]>([]);
-  const keyboardCallback = (keydowns: { code: string, key: string }[], keyups: { code: string, key: string }[], keypresses: { code: string, key: string }[]) => {
-    console.log(`keydowns: ${JSON.stringify(keydowns)}`);
-    console.log(`keyups: ${JSON.stringify(keyups)}`);
-    console.log(`keybuffer: ${JSON.stringify(keypresses)}`);
-    console.log('');
-  }
-
-  React.useEffect(() => {
-    setTimeout(() => {
-      setPresses([
-        { code: 'KeyQ', key: 'q' },
-        { code: 'KeyW', key: 'w' },
-        { code: 'KeyE', key: 'e' },
-        { code: 'KeyR', key: 'r' },
-        { code: 'KeyT', key: 't' },
-      ]);
-      setTimeout(() => {
-        setPresses([
-          { code: 'KeyQ', key: 'q' },
-          { code: 'KeyS', key: 's' },
-          { code: 'KeyE', key: 'e' },
-          { code: 'KeyF', key: 'f' },
-          { code: 'KeyT', key: 't' },
-        ]);
-      }, 5000)
-    }, 5000);
-  }, [])
 
   return (
     <div>
-      <OnscreenKeyboardComponent
-        disabled={['fn']}
-        presses={presses}  
-        callback={keyboardCallback}
-      />
+      <InputOnscreenComponent/>
     </div>
   );
 }
